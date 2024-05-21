5f6d01e30dd549fc27702841cc55b6ab6d508f7b
Benjamin Phillips <benjamin.phillips.webdev@gmail.com>
2024-05-20 18:54:54 -0700

    changed iframe permissions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,9 +17,9 @@ export default function App() {
         return (
             <section className="w-full h-full rounded-b-[45px] rounded-t-xl snap-center flex-none overflow-hidden" id="app" >
                 <iframe 
+                    allow="geolocation 'self' https://bbcs-eight.vercel.app/"
                     src="https://app.bestchoiceroofing.com" 
                     className="w-full h-full"
-                    allow="*"
                 >
                     <p>Your browser does not support iframes.</p>
                 </iframe>
